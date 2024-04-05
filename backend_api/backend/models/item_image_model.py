84e6ac9e2beb51afae055463c2720fc05e55459e
long <nguyenthanhlong@morenergy.net>
2024-04-05 18:05:25 +0200

    refactor: use image field to store image, install Pillow

--- backend_api/backend/models/item_image_model.py
+++ backend_api/backend/models/item_image_model.py
@@ -3,8 +3,10 @@ from django.db import models
 from .item_model import Item
 
 class ItemImage(models.Model):
+    # ID is the UNIX timestamp
     id = models.BigIntegerField(primary_key=True)
     file_name = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='item_images/', null=True, blank=True, default=None)
     device_id = models.ForeignKey(
         Item,
         null= True,
@@ -12,7 +14,5 @@ class ItemImage(models.Model):
         on_delete=models.CASCADE,
         default=None
     )
-
-
     class Meta:
         db_table = 'ItemImages'
--- backend_api/backend/serializers/item_image_serializer.py
+++ backend_api/backend/serializers/item_image_serializer.py
@@ -4,4 +4,4 @@ from ..models import ItemImage
 class ItemImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ItemImage
-        fields = ['id', 'file_name', 'device_id']
+        fields = ['id', 'file_name', 'device_id', 'image']
