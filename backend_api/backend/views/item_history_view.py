163b1af31ba1ef2aee222c30ae0e120afa8ff1bd
long <nguyenthanhlong@morenergy.net>
2024-05-10 17:19:14 +0200

    feat: link borrowed by user id to item history

--- backend_api/backend/views/item_history_view.py
+++ backend_api/backend/views/item_history_view.py
@@ -3,12 +3,46 @@ from ..serializers import ItemHistorySerializer
 from ..models import ItemHistory
 from rest_framework import status
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from ..models import Item
 
 class ItemHistoryView(BaseCRUDAPIView):
     queryset = ItemHistory.objects.all()
     serializer_class = ItemHistorySerializer
     lookup_fields = {'id': 'id', 'item': 'item', 'user': 'user', 'item_history_type': 'item_history_type', 'room': 'room', 'date': 'date'}
-    
+    # Override the post method
+    def post(self, request, *args, **kwargs):
+        # Extract user_id and item_id from POST request data
+        user_id = request.data.get('user')
+        item_id = request.data.get('item')
+
+        # 1 = borrow, 2 = return
+        history_type_id = request.data.get('item_history_type')
+
+        # Check if both user_id and item_id are provided
+        if user_id is None or item_id is None or history_type_id is None:
+            return Response({'error': 'Both user_id and item_id must be provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Retrieve the Item object
+        item = get_object_or_404(Item, pk=item_id)
+
+        # Patch Item table to update borrowed_by_user_id column
+        if history_type_id == 1:
+            item.borrowed_by_user_id = user_id
+        if history_type_id == 2:
+            item.borrowed_by_user_id = None
+
+
+        item.save()
+
+        ### Save history
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Item and ItemHistory updated successfully'}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class GetItemHistoryByItemId(BaseCRUDAPIView):
